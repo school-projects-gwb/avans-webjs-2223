e7afa2e032fb1c08e7924784eb8e094f4fa2b393
gunwunbun <jvanhouts@protonmail.com>
2023-03-04 18:08:27 +0100

    some very basic stuff

---
+++ js/models/package.js
@@ -0,0 +1,40 @@
+class Package {
+    
+    constructor() {
+        this.shape = this.generateRandomShape();
+        this.width = this.calculateWidth();
+        this.height = this.calculateHeight();
+    }
+
+    generateRandomShape() {
+        // All possible Tetromino shapes
+        const shapes = [
+            [[1, 1], [1, 1]], // O-shape
+            [[1], [1], [1], [1]], // I-shape
+            [[1, 0], [1, 0], [1, 1]], // L-shape
+            [[0, 1], [0, 1], [1, 1]], // J-shape
+            [[0, 1], [1, 1], [1, 0]], // S-shape
+            [[1, 0], [1, 1], [0, 1]], // Z-shape
+            [[1, 1, 1], [0, 1, 0]] // T-shape
+        ];
+
+        const index = Math.floor(Math.random() * shapes.length);
+        return shapes[index];
+    }
+
+    calculateWidth() {
+        let maxRowWidth = 0;
+        this.shape.forEach(row => {
+            const rowWidth = row.length;
+            if (rowWidth > maxRowWidth) {
+                maxRowWidth = rowWidth;
+            }
+        });
+
+        return maxRowWidth;
+    }
+
+    calculateHeight() {
+        return this.shape.length;
+    }
+}
